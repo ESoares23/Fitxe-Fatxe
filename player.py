3bb8afc0e209ec6b3f8b5a6aaf22ab29ccbbd6e8
esoares23 <elizaniosoares13gmail.com>
2024-06-13 01:39:10 -0100

    adicao de novas funcionalidades

--- player.py
+++ player.py
@@ -5,43 +5,150 @@ from tkinter import messagebox
 import threading
 
 class TicTacToeClient(tk.Tk):
-    def __init__(self, player, server_ip, server_port):
+    def __init__(self, server_ip, server_port):
         super().__init__()
-        self.title("Tic Tac Toe")
-        self.player = player
-        self.current_turn = 'X'  # O jogo começa com o jogador 'X'
+        self.title("Fitxe-Fatxe")
         self.server_ip = server_ip
         self.server_port = server_port
+        self.player = None
+        self.current_turn = 'X'
+        self.last_winner = 'X'  # Para armazenar o vencedor do último jogo
+        self.client_socket = None
         self.buttons = [[None for _ in range(3)] for _ in range(3)]
         self.board = [['' for _ in range(3)] for _ in range(3)]
-        self.create_widgets()
-        self.connect_to_server()
+        self.logged_in = False
+        self.create_register_widgets()
+        
+    def create_login_widgets(self):
+            self.clear_widgets()
+            self.username_label = tk.Label(self, text="nome:")
+            self.username_label.grid(row=0, column=0)
+            self.username_entry = tk.Entry(self)
+            self.username_entry.grid(row=0, column=1)
+            
+            self.password_label = tk.Label(self, text="Password:")
+            self.password_label.grid(row=1, column=0)
+            self.password_entry = tk.Entry(self, show="*")
+            self.password_entry.grid(row=1, column=1)
+            
+            self.login_button = tk.Button(self, text="Login", command=self.login)
+            self.login_button.grid(row=2, column=0, columnspan=2)
+            
+            self.register_link = tk.Label(self, text="Não tem conta? Registre aqui.", fg="blue", cursor="hand2")
+            self.register_link.grid(row=3, column=0, columnspan=2)
+            self.register_link.bind("<Button-1>", lambda e: self.create_register_widgets())
 
-    def create_widgets(self):
+    def create_register_widgets(self):
+        self.clear_widgets()
+        self.username_label = tk.Label(self, text="nome de Usuario:")
+        self.username_label.grid(row=0, column=0)
+        self.username_entry = tk.Entry(self)
+        self.username_entry.grid(row=0, column=1)
+        
+        self.password_label = tk.Label(self, text="Password:")
+        self.password_label.grid(row=1, column=0)
+        self.password_entry = tk.Entry(self, show="*")
+        self.password_entry.grid(row=1, column=1)
+        
+        self.register_button = tk.Button(self, text="Registar", command=self.register)
+        self.register_button.grid(row=2, column=0, columnspan=2)
+        
+        self.login_link = tk.Label(self, text="Já tem conta? Faça Login aqui.", fg="blue", cursor="hand2")
+        self.login_link.grid(row=3, column=0, columnspan=2)
+        self.login_link.bind("<Button-1>", lambda e: self.create_login_widgets())
+
+   
+    def clear_widgets(self):
+        for widget in self.winfo_children():
+            widget.grid_remove()
+
+    def create_game_widgets(self):
+        self.clear_widgets()
         for i in range(3):
             for j in range(3):
                 self.buttons[i][j] = tk.Button(self, text='', font='normal 20 bold', width=5, height=2,
                                                command=lambda i=i, j=j: self.make_move(i, j))
                 self.buttons[i][j].grid(row=i, column=j)
-
+        
         self.message_list = tk.Listbox(self, height=5, width=50)
         self.message_list.grid(row=3, column=0, columnspan=3)
 
         self.message_entry = tk.Entry(self, width=50)
         self.message_entry.grid(row=4, column=0, columnspan=2)
 
-        self.send_button = tk.Button(self, text="Send", command=self.send_message)
+        self.send_button = tk.Button(self, text="Enviar", command=self.send_message)
         self.send_button.grid(row=4, column=2)
+        self.create_ranking_table()
+        
+    def create_ranking_table(self):
+        self.ranking_frame = tk.Frame(self)
+        self.ranking_frame.grid(row=5, column=0, columnspan=3, pady=10)
 
-    def connect_to_server(self):
+        tk.Label(self.ranking_frame, text="Ranking", font="bold").grid(row=0, column=0, columnspan=4)
+
+        # Cabeçalho da tabela
+        tk.Label(self.ranking_frame, text="Rank", font="bold").grid(row=1, column=0)
+        tk.Label(self.ranking_frame, text="Player", font="bold").grid(row=1, column=1)
+        tk.Label(self.ranking_frame, text="Wins", font="bold").grid(row=1, column=2)
+        tk.Label(self.ranking_frame, text="Draws", font="bold").grid(row=1, column=3)
+        tk.Label(self.ranking_frame, text="Points", font="bold").grid(row=1, column=4)
+
+   
+    def register(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+
+        if not username or not password:
+            messagebox.showerror("Erro ao registar", "Por faavor insira nome e password! ")
+            return
+
         try:
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.client_socket.connect((self.server_ip, self.server_port))
-            self.client_socket.send((json.dumps({'action': 'join', 'player': self.player}) + "\n").encode('utf-8'))
-            threading.Thread(target=self.receive_messages).start()
+            register_data = {'action': 'register', 'username': username, 'password': password}
+            self.client_socket.send((json.dumps(register_data) + "\n").encode('utf-8'))
+            
+            response = self.client_socket.recv(1024).decode('utf-8').strip()
+            response_data = json.loads(response)
+
+            if response_data['status'] == 'success':
+                messagebox.showinfo("Registrado com sucesso", "Por favor faça Login!")
+                self.create_login_widgets()
+            else:
+                messagebox.showerror("Erro de registro", response_data['message'])
+                self.client_socket.close()
+
         except Exception as e:
-            messagebox.showerror("Error", f"Could not connect to server: {e}")
-            self.destroy()
+            messagebox.showerror("Connection Error", f"Não foi possivel conectar com o servidor: {e}")
+
+    def login(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+
+        if not username or not password:
+            messagebox.showerror("Erro de Login","Por faavor insira o nome e a password! ")
+            return
+
+        try:
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_socket.connect((self.server_ip, self.server_port))
+            login_data = {'action': 'login', 'username': username, 'password': password}
+            self.client_socket.send((json.dumps(login_data) + "\n").encode('utf-8'))
+            
+            response = self.client_socket.recv(1024).decode('utf-8').strip()
+            response_data = json.loads(response)
+
+            if response_data['status'] == 'success':
+                self.player = response_data['player']
+                self.logged_in = True
+                self.create_game_widgets()
+                threading.Thread(target=self.receive_messages).start()
+            else:
+                messagebox.showerror("Login Error", response_data['message'])
+                self.client_socket.close()
+
+        except Exception as e:
+            messagebox.showerror("Connection Error", f"Não foi possivel conectar com o servidor: {e}")
 
     def make_move(self, i, j):
         if self.board[i][j] == '' and self.player == self.current_turn:
@@ -53,9 +160,28 @@ class TicTacToeClient(tk.Tk):
             }
             try:
                 self.client_socket.send((json.dumps(move) + "\n").encode('utf-8'))
-            except ConnectionAbortedError as e:
-                print("Error:", e)
-                # Adicione qualquer outra ação necessária para lidar com o erro de conexão
+            except Exception as e:
+                messagebox.showerror("Connection Error", f"Error: {e}")
+    
+    def update_ranking_table(self, ranking):
+        for widget in self.ranking_frame.winfo_children():
+            widget.grid_remove()
+
+        tk.Label(self.ranking_frame, text="Ranking", font="bold").grid(row=0, column=0, columnspan=4)
+
+        # Cabeçalho da tabela
+        tk.Label(self.ranking_frame, text="Rank", font="bold").grid(row=1, column=0)
+        tk.Label(self.ranking_frame, text="Player", font="bold").grid(row=1, column=1)
+        tk.Label(self.ranking_frame, text="Wins", font="bold").grid(row=1, column=2)
+        tk.Label(self.ranking_frame, text="Draws", font="bold").grid(row=1, column=3)
+        tk.Label(self.ranking_frame, text="Points", font="bold").grid(row=1, column=4)
+
+        for i, user in enumerate(ranking):
+            tk.Label(self.ranking_frame, text=user['rank']).grid(row=i+2, column=0)
+            tk.Label(self.ranking_frame, text=user['username']).grid(row=i+2, column=1)
+            tk.Label(self.ranking_frame, text=user['wins']).grid(row=i+2, column=2)
+            tk.Label(self.ranking_frame, text=user['draws']).grid(row=i+2, column=3)
+            tk.Label(self.ranking_frame, text=user['points']).grid(row=i+2, column=4)
 
     def receive_messages(self):
         buffer = ""
@@ -77,9 +203,40 @@ class TicTacToeClient(tk.Tk):
                         self.reset_board()
                     elif data['action'] == 'turn':
                         self.update_turn(data['player'])
+                    elif data['action'] == 'user_info':
+                        self.update_user_info(data['user_info'])
+                    elif data['action'] == 'ranking':
+                        self.update_ranking(data['ranking'])
             except Exception as e:
-                print("Error receiving message:", e)
+                messagebox.showerror("Connection Error", f"Error receiving message: {e}")
                 break
+
+    def update_ranking(self, ranking):
+        # Atualiza a tabela de ranking com base nos dados recebidos
+        self.clear_ranking_table()
+        for i, user in enumerate(ranking):
+            tk.Label(self.ranking_frame, text=user['username']).grid(row=i+2, column=0)
+            tk.Label(self.ranking_frame, text=user['wins']).grid(row=i+2, column=1)
+            tk.Label(self.ranking_frame, text=user['draws']).grid(row=i+2, column=2)
+            tk.Label(self.ranking_frame, text=user['points']).grid(row=i+2, column=3)
+
+    def clear_ranking_table(self):
+        for widget in self.ranking_frame.winfo_children():
+            widget.destroy()
+        tk.Label(self.ranking_frame, text="Ranking", font="bold").grid(row=0, column=0, columnspan=3)
+        tk.Label(self.ranking_frame, text="Jogador", font="bold").grid(row=1, column=0)
+        tk.Label(self.ranking_frame, text="Vitorias(A)", font="bold").grid(row=1, column=1)
+        tk.Label(self.ranking_frame, text="Empates", font="bold").grid(row=1, column=2)
+        tk.Label(self.ranking_frame, text="Pts", font="bold").grid(row=1, column=3)
+
+
+    def update_user_info(self, user_info):
+        self.username = user_info['username']
+        self.wins = user_info['wins']
+        self.points = user_info['points']
+        self.draws = user_info['draws']
+        self.title(f"Tic Tac Toe - Player {self.username} - Wins: {self.wins}, Points: {self.points}, Draws: {self.draws}")
+
 
     def update_messages(self, message):
         self.message_list.insert(tk.END, message)
@@ -99,9 +256,8 @@ class TicTacToeClient(tk.Tk):
             }
             try:
                 self.client_socket.send((json.dumps(msg) + "\n").encode('utf-8'))
-            except ConnectionAbortedError as e:
-                print("Error:", e)
-                # Adicione qualquer outra ação necessária para lidar com o erro de conexão
+            except Exception as e:
+                messagebox.showerror("Connection Error", f"Error: {e}")
             self.message_entry.delete(0, tk.END)
 
     def show_winner(self, winner):
@@ -109,22 +265,22 @@ class TicTacToeClient(tk.Tk):
             messagebox.showinfo("Game Over", "It's a draw!")
         else:
             messagebox.showinfo("Game Over", f"Player {winner} wins!")
+            self.last_winner = winner  # Armazenar o vencedor do jogo
         self.reset_board()
 
     def reset_board(self):
         self.board = [['' for _ in range(3)] for _ in range(3)]
+        self.current_turn = self.last_winner if self.last_winner != 'Draw' else 'X'  # Definir o turno inicial com base no vencedor anterior ou 'X' se empate
         self.update_buttons()
+        self.title(f"Tic Tac Toe - Player {self.player} - {self.current_turn}'s turn")
+
 
     def update_turn(self, player):
         self.current_turn = player
         self.title(f"Tic Tac Toe - Player {self.player} - {player}'s turn")
 
 if __name__ == "__main__":
-    player = input("Choose your player (X or O): ").upper()
-    if player not in ['X', 'O']:
-        print("Invalid player choice. Please choose X or O.")
-    else:
-        server_ip = input("Enter server IP: ")
-        server_port = int(input("Enter server port: "))
-        app = TicTacToeClient(player, server_ip, server_port)
-        app.mainloop()
+    server_ip = input("Enter server IP: ")
+    server_port = int(input("Enter server port: "))
+    app = TicTacToeClient(server_ip, server_port)
+    app.mainloop()
--- server_fitxe-fatxe.py
+++ server_fitxe-fatxe.py
@@ -3,9 +3,19 @@ import threading
 import json
 import redis
 import time
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Configuração do Redis
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+
+# Configuração do MongoDB
+mongo_client = MongoClient("mongodb://localhost:27017/")
+db = mongo_client['tictactoe_db']
+users_collection = db['users']
+users_collection.update_many({}, {'$set': {'draws': 0}})
+users_collection.update_many({}, {'$set': {'points': 0}})
+games_collection = db['games']
 
 class TicTacToeServer:
     def __init__(self, host='0.0.0.0', port=65432):
@@ -14,12 +24,11 @@ class TicTacToeServer:
         self.server_socket.listen(2)
         print("Server started, waiting for players...")
         self.clients = []
+        self.connected_users = {}  # Dicionário para rastrear usuários conectados
         self.game_id = "tictactoe_game"
         self.lock = threading.Lock()
-        self.expiration_time = 60  # Expirar dados em 60 segundos
-        self.current_player = 'X'  # Jogador que começa o jogo
-
-        # Thread para expirar dados periodicamente
+        self.expiration_time = 120  
+        self.current_player = 'X'
         threading.Thread(target=self.expire_cache_periodically, daemon=True).start()
 
     def expire_cache_periodically(self):
@@ -36,7 +45,11 @@ class TicTacToeServer:
                 if not msg:
                     break
                 data = json.loads(msg)
-                if data['action'] == 'move':
+                if data['action'] == 'register':
+                    self.handle_register(data, client_socket)
+                elif data['action'] == 'login':
+                    self.handle_login(data, client_socket)
+                elif data['action'] == 'move':
                     self.handle_move(data, player)
                 elif data['action'] == 'message':
                     self.handle_message(data)
@@ -45,10 +58,56 @@ class TicTacToeServer:
                 break
         client_socket.close()
 
+    def handle_login(self, data, client_socket):
+        username = data['username']
+        password = data['password']
+        user = users_collection.find_one({'username': username})
+        if user and check_password_hash(user['password'], password):
+            player = 'X' if len(self.clients) == 0 else 'O'
+            response = {'status': 'success', 'player': player}
+            self.clients.append(client_socket)
+            self.connected_users[username] = client_socket  # Adiciona o usuário à lista de conectados
+            users_collection.update_one({'username': username}, {'$set': {'socket': client_socket.getpeername()}})
+            client_socket.send((json.dumps(response) + "\n").encode('utf-8'))
+            self.send_user_info(client_socket)
+            self.send_ranking()  # Envia o ranking atualizado
+        else:
+            response = {'status': 'error', 'message': 'Invalid username or password.'}
+            client_socket.send((json.dumps(response) + "\n").encode('utf-8'))
+
+    def handle_register(self, data, client_socket):
+        username = data['username']
+        password = data['password']
+        if users_collection.find_one({'username': username}):
+            response = {'status': 'error', 'message': 'Username already exists.'}
+        else:
+            hashed_password = generate_password_hash(password)
+            users_collection.insert_one({'username': username, 'password': hashed_password, 'wins': 0, 'points': 0, 'draws': 0})
+            response = {'status': 'success', 'message': 'Registration successful.'}
+        client_socket.send((json.dumps(response) + "\n").encode('utf-8'))
+
+   
+    def send_user_info(self, client_socket):
+        try:
+            client_info = client_socket.getpeername()
+            username = users_collection.find_one({'socket': client_info})['username']
+            user_info = users_collection.find_one({'username': username}, {'_id': 0, 'username': 1, 'wins': 1, 'points': 1, 'draws': 1})
+            client_socket.send((json.dumps({'action': 'user_info', 'user_info': user_info}) + "\n").encode('utf-8'))
+        except Exception as e:  
+            print(f"Error sending user info: {e}")
+            
+    def send_ranking(self):
+        try:
+            connected_usernames = list(self.connected_users.keys())
+            users = list(users_collection.find({'username': {'$in': connected_usernames}}, {'_id': 0, 'username': 1, 'wins': 1, 'points': 1, 'draws': 1}).sort([('points', -1), ('wins', -1)]))
+            ranking = [{'rank': i + 1, 'username': user['username'], 'wins': user['wins'], 'points': user['points'], 'draws': user['draws']} for i, user in enumerate(users)]
+            self.broadcast({'action': 'ranking', 'ranking': ranking})
+        except Exception as e:
+            print(f"Error sending ranking: {e}")
+
     def handle_move(self, data, player):
         with self.lock:
             if player != self.current_player:
-                # Não é a vez deste jogador
                 return
 
             board = json.loads(redis_client.get(self.game_id) or json.dumps([['', '', ''], ['', '', ''], ['', '', '']]))
@@ -60,9 +119,9 @@ class TicTacToeServer:
                 winner = self.check_winner(board)
                 if winner:
                     self.broadcast({'action': 'game_over', 'winner': winner})
-                    self.reset_board()
+                    self.update_winner(winner)
+                    self.reset_board()  # Remover o argumento aqui
                 else:
-                    # Alterna para o próximo jogador
                     self.current_player = 'O' if self.current_player == 'X' else 'X'
                     self.broadcast({'action': 'turn', 'player': self.current_player})
 
@@ -97,19 +156,36 @@ class TicTacToeServer:
 
         return None
 
+    def update_winner(self, winner):
+        try:
+            if winner in ['X', 'O']:
+                player_index = 0 if winner == 'X' else 1
+                client_socket = self.clients[player_index]
+                client_info = client_socket.getpeername()
+                username = users_collection.find_one({'socket': client_info})['username']
+                users_collection.update_one({'username': username}, {'$inc': {'wins': 1, 'points': 3}})
+                print(f"Updated {username}'s points and wins for winning.")
+            elif winner == 'Draw':
+                for client_socket in self.clients:
+                    client_info = client_socket.getpeername()
+                    username = users_collection.find_one({'socket': client_info})['username']
+                    users_collection.update_one({'username': username}, {'$inc': {'points': 1, 'draws': 1}})
+                    print(f"Updated {username}'s points and draws for draw.")
+            self.send_ranking()  # Enviar ranking após atualização dos pontos
+        except Exception as e:
+            print(f"Error updating winner in database: {e}")
+
     def reset_board(self):
-        # Reiniciar o tabuleiro
         redis_client.set(self.game_id, json.dumps([['', '', ''], ['', '', ''], ['', '', '']]))
-        self.current_player = 'X'  # Reiniciar o jogador inicial
         self.broadcast({'action': 'reset'})
 
     def start(self):
         while True:
             client_socket, addr = self.server_socket.accept()
-            print(f"Player connected from {addr}")
-            self.clients.append(client_socket)
-            player = 'X' if len(self.clients) == 1 else 'O'
-            threading.Thread(target=self.handle_client, args=(client_socket, player)).start()
+            print(f"Connection from {addr} has been established.")
+            player = 'X' if len(self.clients) == 0 else 'O'
+            client_thread = threading.Thread(target=self.handle_client, args=(client_socket, player))
+            client_thread.start()
 
 if __name__ == "__main__":
     server = TicTacToeServer()
